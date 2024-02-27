2906991b2e0867f07833c907070a57534ab088dd
Dan <danretegan@yahoo.com>
2024-02-27 14:07:43 +0200

    Update operations.js

--- my-app/src/redux/operations.js
+++ my-app/src/redux/operations.js
@@ -3,7 +3,17 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
 
-export const fetchTasks = createAsyncThunk("tasks/fetchAll", async () => {
-  const response = await axios.get("/tasks");
-  return response.data;
-});
+export const fetchTasks = createAsyncThunk(
+  "tasks/fetchAll",
+  async (_, thunkAPI) => {
+    try {
+      const response = await axios.get("/tasks");
+
+      // In cazul unei cereri cu succes, vom returna un promise cu date:
+      return response.data;
+    } catch (error) {
+      // Daca cererea esueaza, returnam un promise care va fi respins si va contine textul erorii:
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
