70e76c119f538595f228f532728ff1ce6d267071
Dan <danretegan@yahoo.com>
2024-02-27 13:31:28 +0200

    refactoring operations.js with createAsyncThunk

--- my-app/src/redux/operations.js
+++ my-app/src/redux/operations.js
@@ -1,24 +1,9 @@
 import axios from "axios";
-import {
-  fetchingError,
-  fetchingSuccess,
-  fetchingInProgress,
-} from "./tasksSlice";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
 axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
 
-export const fetchTasks = () => async (dispatch) => {
-  try {
-    // Indicator de loading:
-    dispatch(fetchingInProgress());
-
-    // HTTP get:
-    const response = await axios.get("/tasks");
-
-    // Procesarea datelor:
-    dispatch(fetchingSuccess(response.data));
-  } catch (error) {
-    // Procesarea erorilor:
-    dispatch(fetchingError(error.message));
-  }
-};
+export const fetchTasks = createAsyncThunk("tasks/fetchAll", async () => {
+  const response = await axios.get("/tasks");
+  return response.data;
+});
