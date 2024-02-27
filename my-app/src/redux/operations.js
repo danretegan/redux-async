47a75ea88b8c9afdb0e5444177fde3e9e88e71d7
Dan <danretegan@yahoo.com>
2024-02-27 12:07:20 +0200

    operations.js update
    redux-thunk instalation

--- my-app/src/redux/operations.js
+++ my-app/src/redux/operations.js
@@ -1,9 +1,24 @@
 import axios from "axios";
+import {
+  fetchingError,
+  fetchingSuccess,
+  fetchinhInProgress,
+} from "./tasksSlice";
 
 axios.defaults.baseURL = "https://62584f320c918296a49543e7.mockapi.io";
 
-const fetchTasks = () => async (dispatch) => {
+export const fetchTasks = () => async (dispatch) => {
   try {
+    // Indicator de loading:
+    dispatch(fetchinhInProgress());
+
+    // HTTP get:
     const response = await axios.get("/tasks");
-  } catch (error) {}
+
+    // Procesarea datelor:
+    dispatch(fetchingSuccess(response.data));
+  } catch (error) {
+    // Procesarea erorilor:
+    dispatch(fetchingError(error.message));
+  }
 };
