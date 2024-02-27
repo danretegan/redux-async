c013c4e413c63cd829112288383f50a150d68ff6
Dan <danretegan@yahoo.com>
2024-02-27 13:52:40 +0200

    Update tasksSlice.js

--- my-app/src/redux/tasksSlice.js
+++ my-app/src/redux/tasksSlice.js
@@ -1,30 +1,18 @@
 import { initialState } from "./initialState";
 import { createSlice } from "@reduxjs/toolkit";
+// Importam operatia:
+import { fetchTasks } from "./operations";
 
 const tasksSlice = createSlice({
   name: "tasks",
   initialState: initialState,
-  reducers: {
-    // Este exectuat la inceputul unei solicitari HTTP:
-    fetchingInProgress(state) {
-      state.isLoading = true;
-    },
 
-    // Va fi executat daca solicitarea HTTP  a avut succes:
-    fetchingSuccess(state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.items = action.payload;
-    },
-
-    // Va fi executat dac solicitarea HTTP s-a incheiat cu eroare:
-    fetchingError(state, action) {
-      state.isLoading = false;
-      state.error = action.payload;
-    },
+  // Adaugam gestionarea actiunilor externe:
+  extraReducers: {
+    [fetchTasks.pending](state, action) {},
+    [fetchTasks.fulfilled](state, action) {},
+    [fetchTasks.rejected](state, action) {},
   },
 });
 
-export const { fetchingInProgress, fetchingSuccess, fetchingError } =
-  tasksSlice.actions;
 export const tasksReducer = tasksSlice.reducer;
